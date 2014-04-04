30b2e1bd09db47cfa2de2184b3b0b40a61259c11
Matthieu Hanne <matthieu.hanne@gmail.com>
2014-04-04 10:53:07 +0200

    Set up router

---
+++ app/router.js
@@ -0,0 +1,31 @@
+/* Post index*/
+define(['app', 'backbone'], function(App, Backbone) {
+    'use strict';
+
+	var Router = {
+		'initialize': function(){
+			var routes = {
+				'routes':{ 
+					'': 'index',
+					'destockage': 'destockage',
+					'services': 'services',
+					'peinture': 'peinture',
+					'electricite': 'electricite',
+					'diaspora': 'diaspora',
+					'contacts': 'contacts',
+				},
+				'index': function(){},
+				'destockage': function(){},
+				'services': function(){},
+				'peinture': function(){},
+				'electricite': function(){},
+				'diaspora': function(){},
+				'contacts': function(){},
+			};
+
+		    new (Backbone.Router.extend(routes))();
+	    }}; 
+    return Router;
+});
+
+
